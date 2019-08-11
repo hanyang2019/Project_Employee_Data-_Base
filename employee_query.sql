9df08cd6f4c7dbe3a786f424b72e4ae03ef4134c
Han Yang <ilovebabycat2002@hotmail.com>
2019-08-10 22:22:54 -0500

    fix #8 order by last name count

--- employee_query.sql
+++ employee_query.sql
@@ -42,7 +42,7 @@ WHERE dept_name='Sales' OR dept_name='Development';
 --8.In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 SELECT DISTINCT last_name, Count(last_name) FROM employees
 GROUP BY last_name
-ORDER BY last_name DESC;
+ORDER BY Count(last_name) DESC;
 
 --Bonus Average salary by title
 SELECT title, ROUND(AVG(salary::numeric),2) AS "Average Salary"
